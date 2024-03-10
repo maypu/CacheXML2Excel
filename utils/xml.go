a77f9cc41b9caa9923629aa78e5af73d97828177
maypu <peng5649360@126.com>
2024-03-10 16:15:40 +0800

    fix:获取描述为空时取全部描述

--- utils/xml.go
+++ utils/xml.go
@@ -8,20 +8,20 @@ import (
 )
 
 type CacheTableClass struct {
-	Name	string
-	Description string
-	ClassType	string
-	SqlTableName	string
-	Property    []CacheTableProperty
+	Name         string
+	Description  string
+	ClassType    string
+	SqlTableName string
+	Property     []CacheTableProperty
 }
 
 type CacheTableProperty struct {
 	Name         string
 	Description  string
-	Type		string
-	ChineseType	string
+	Type         string
+	ChineseType  string
 	SqlFieldName string
-	PointerTable	string	//类型为指针时，指向的表名
+	PointerTable string //类型为指针时，指向的表名
 }
 
 func ReadXml(path string) CacheTableClass {
@@ -34,10 +34,10 @@ func ReadXml(path string) CacheTableClass {
 
 	var cacheTableClass CacheTableClass
 	//类名
-	cacheTableClass.Name = cacheClass.SelectAttrValue("name","")
+	cacheTableClass.Name = cacheClass.SelectAttrValue("name", "")
 	//计算表名
 	cacheTableClassName := strings.Replace(cacheTableClass.Name, ".", "_", -1)
-	cacheTableClassName = ReplaceRight(cacheTableClassName,"_",".",1)
+	cacheTableClassName = ReplaceRight(cacheTableClassName, "_", ".", 1)
 	cacheTableClass.SqlTableName = cacheTableClassName
 	//描述
 	if classDescription := cacheClass.SelectElement("Description"); classDescription != nil {
@@ -52,7 +52,7 @@ func ReadXml(path string) CacheTableClass {
 	for _, property := range cacheClass.SelectElements("Property") {
 
 		if cardinality := property.SelectElement("Cardinality"); cardinality != nil {
-			if cardinality.Text() == "children" {		//父表中指向子表的字段可以不算
+			if cardinality.Text() == "children" { //父表中指向子表的字段可以不算
 				continue
 			}
 		}
@@ -80,7 +80,7 @@ func ReadXml(path string) CacheTableClass {
 		//字段指针表
 		if cacheTableProperty.ChineseType == "指针" {
 			pointerTable := strings.Replace(cacheTableProperty.Type, ".", "_", -1)
-			pointerTable = ReplaceRight(pointerTable,"_",".",1)
+			pointerTable = ReplaceRight(pointerTable, "_", ".", 1)
 			cacheTableProperty.PointerTable = pointerTable
 		}
 		cacheTableClass.Property = append(cacheTableClass.Property, cacheTableProperty)
@@ -93,29 +93,33 @@ func ExtractTableDesc(description string) string {
 	var desc string
 	reg := regexp.MustCompile(`描述: (?s:(.*?))\n`)
 	descArr := reg.FindStringSubmatch(description)
-	if len(descArr)>1 {
+	if len(descArr) > 1 {
 		desc = descArr[1]
 	}
-	desc = strings.Replace(desc,"\n", "", -1)
+	if len(desc) >0 {
+		desc = strings.Replace(desc, "\n", "", -1)
+	} else {
+		desc = description
+	}
 	return desc
 }
 
 func PropertyTypeToChinese(propertyType string) string {
-	propertyTypeChinese := map[string] string {
-		"%String": "字符串",
-		"%Library.String": "字符串",
-		"%Integer": "数字",
+	propertyTypeChinese := map[string]string{
+		"%String":          "字符串",
+		"%Library.String":  "字符串",
+		"%Integer":         "数字",
 		"%Library.Integer": "数字",
-		"%Boolean": "布尔",
+		"%Boolean":         "布尔",
 		"%Library.Boolean": "布尔",
-		"%Date": "日期",
-		"%Library.Date": "日期",
-		"%Time": "时间",
-		"%Library.Time": "时间",
-		"%List": "列表",
-		"%Library.List": "列表",
-		"%Float": "浮点数",
-		"%Library.Float": "浮点数",
+		"%Date":            "日期",
+		"%Library.Date":    "日期",
+		"%Time":            "时间",
+		"%Library.Time":    "时间",
+		"%List":            "列表",
+		"%Library.List":    "列表",
+		"%Float":           "浮点数",
+		"%Library.Float":   "浮点数",
 	}
 	if value, isMapContainsKey := propertyTypeChinese[propertyType]; isMapContainsKey {
 		//key exist
